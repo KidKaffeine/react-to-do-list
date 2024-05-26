bcec159861c6bc5ea82e2ad781872f874443d180
KidKaffeine <vanessa.m.a1989@gmail.com>
2024-05-26 18:33:26 +0200

    created reusable card component, aded paragraph to header and grid layout properties to card header and children

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,5 +1,15 @@
 import styles from "./Header.module.css";
+import Card from "../UI/Card/Card";
 
 export default function Header({ title }) {
-  return <h1 className={styles.appTitle}>{title}</h1>;
+  return (
+    <>
+      <Card className={"headerCard"}>
+        <h1 className={styles.appTitle}>{title}</h1>
+        <p className={styles.paragraphHeader}>
+          (we apologise <em className={styles.paragraphEm}>sincerily</em> for the swearing)
+        </p>
+      </Card>
+    </>
+  );
 }
