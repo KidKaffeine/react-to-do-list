29d4604edf2d6e021e95f1bd30a2f793f6bb1764
KidKaffeine <vanessa.m.a1989@gmail.com>
2024-05-26 15:46:20 +0200

    added background color to app, created header component, rendered it on app, added font to title

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import './App.css';
+import Header from './components/Header/Header';
 
 function App() {
   return (
     <div className="App">
+      <Header title={"shit to do"}/>
     </div>
   );
 }
