dd60065e193229261f5a2948e214e549cacd8916
KidKaffeine <vanessa.m.a1989@gmail.com>
2024-05-26 17:17:59 +0200

    created ul and li with icons in navbar, created li reusable component, moved flex box properties to app instead of body

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,10 +1,39 @@
 import styles from "./Navbar.module.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faPlusCircle, faStar, faMagnifyingGlass } from "@fortawesome/free-solid-svg-icons";
+import ListItem from "../UI/ListItem/ListItem";
 
 export default function Navbar() {
+
+
   return (
     <>
       <nav className={styles.navBar}>
-        <h2> A navbar</h2>
+        <ul className={styles.navList}>
+          <ListItem key="starIcon">
+            <FontAwesomeIcon
+              icon={faStar}
+              size="3x"
+              className={styles.navIcons}
+            />
+          </ListItem>
+          <ListItem key="addIcon">
+            <FontAwesomeIcon
+              icon={faPlusCircle}
+              size="3x"
+              beatFade
+              className={styles.navIcons}
+              color="yellow"
+            />
+          </ListItem>
+          <ListItem key="searchIcon">
+            <FontAwesomeIcon
+              icon={faMagnifyingGlass}
+              size="3x"
+              className={styles.navIcons}
+            />
+          </ListItem>
+        </ul>
       </nav>
     </>
   );
