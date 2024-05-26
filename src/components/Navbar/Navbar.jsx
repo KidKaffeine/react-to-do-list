feedad6c93358fc3dac5951f7ad0844825a2d6cf
KidKaffeine <vanessa.m.a1989@gmail.com>
2024-05-26 16:09:26 +0200

    created navbar component, made it fixed to vp bottom, rendered it in app,  installed fontawesome packages

---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,11 @@
+import styles from "./Navbar.module.css";
+
+export default function Navbar() {
+  return (
+    <>
+      <nav className={styles.navBar}>
+        <h2> A navbar</h2>
+      </nav>
+    </>
+  );
+}
