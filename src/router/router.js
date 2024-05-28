9a78b7a8490738c0f96abdf50e4fe70a65d50e9c
KidKaffeine <vanessa.m.a1989@gmail.com>
2024-05-28 10:50:25 +0200

    created error page and styled it

--- src/router/router.js
+++ src/router/router.js
@@ -5,13 +5,14 @@ import {
 } from "react-router-dom";
 import Root from "../pages/root";
 import AddTask from "../pages/addTask";
+import Error from "../pages/error";
 import { handleAddTask } from "../actions";
 
 //default error page
 
 const router = createBrowserRouter(
   createRoutesFromElements(
-    <Route path="/" element={<Root />}>
+    <Route path="/" element={<Root />} errorElement={<Error />}>
       <Route path="addTask" element={<AddTask />} action={handleAddTask} />
     </Route>
   )
