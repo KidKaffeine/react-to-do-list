af8595f3ae80602b5b24215ceefe3c591c8fd256
KidKaffeine <vanessa.m.a1989@gmail.com>
2024-05-29 18:14:27 +0200

    --no-edit

--- src/actions/index.js
+++ src/actions/index.js
@@ -2,6 +2,5 @@ import { redirect } from "react-router-dom"
 
 export const handleAddTask = async ({ request }) => {
 const data = Object.fromEntries(await request.formData())
-console.log(data)
 return redirect("/")
 }
